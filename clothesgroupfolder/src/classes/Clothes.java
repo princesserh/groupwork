99c0011e132f2b84579c462b1f775fb468b9622e
lambypy <danbud@hotmail.co.uk>
2024-10-29 13:55:48 +0000

    Added getters and setters in Clothes

--- clothesgroupfolder/src/classes/Clothes.java
+++ clothesgroupfolder/src/classes/Clothes.java
@@ -4,8 +4,25 @@ public abstract class Clothes {
     private ClothingMaterial material;
     private ClothingColor color;
     private ClothingStyle style;
-
     // private size
+
+
+    public ClothingMaterial getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(ClothingMaterial material) {
+        this.material = material;
+    }
+
+    public ClothingColor getColor() {
+        return color;
+    }
+
+    public void setColor(ClothingColor color) {
+        this.color = color;
+    }
+
 
     public ClothingStyle getStyle() {
         return style;
