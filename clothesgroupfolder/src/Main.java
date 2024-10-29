77d9241eb26a869fec0fd493a206a599a1f79b19
lambypy <danbud@hotmail.co.uk>
2024-10-29 14:37:08 +0000

    Created an array to show polymorphism

--- clothesgroupfolder/src/Main.java
+++ clothesgroupfolder/src/Main.java
@@ -1,6 +1,4 @@
-import classes.Shirt;
-import classes.Clothes;
-import classes.ClothingStyle;
+import classes.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,9 +7,16 @@ public class Main {
 
     public static void queries(){
         Shirt s1 = new Shirt();
-        System.out.println(s1.giveClothingInfo());
+        //System.out.println(s1.giveClothingInfo());
 
+        Trousers t1 = new Trousers(10);
+        Shirt s2 = new Shirt();
+        s2.setColor(ClothingColor.BLACK);
 
+        Clothes[] clothes = {s1, t1, s2};
+        for (Clothes c: clothes){
+            System.out.println(c);
+            System.out.println(c.giveClothingInfo());
+        }
     }
-
 }
