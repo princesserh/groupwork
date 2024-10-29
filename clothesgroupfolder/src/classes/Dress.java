a1efb807c64f18bb1f76e0561bfb7f26efe507ac
princess erhunmwunsee <perhunmwunsee@hotmail.co.uk>
2024-10-29 14:48:14 +0000

    EDIT: Override clothes class

--- clothesgroupfolder/src/classes/Dress.java
+++ clothesgroupfolder/src/classes/Dress.java
@@ -4,6 +4,11 @@ public class Dress extends Clothes {
     private int dressLength;
 
     private int fitting;
-    
+
     private int style;
+
+    @Override
+    public String giveClothingInfo(){
+        return "This item is a dress";
+    }
 }
