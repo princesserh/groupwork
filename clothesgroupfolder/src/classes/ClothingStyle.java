6838a6945b2313ef9aef58aa9e0ba65208741b96
lambypy <danbud@hotmail.co.uk>
2024-10-29 12:32:36 +0000

    Added ClothingStyle enum

---
+++ clothesgroupfolder/src/classes/ClothingStyle.java
@@ -0,0 +1,14 @@
+package classes;
+
+public enum ClothingStyle {
+    CASUAL,
+    FORMAL,
+    ATHLETIC,
+    VINTAGE,
+    BUSINESS_CASUAL,
+    BOHEMIAN,
+    STREETWEAR,
+    PREPPY,
+    GRUNGE,
+    MINIMALIST
+}
--- clothesgroupfolder/src/classes/Shirt.java
+++ clothesgroupfolder/src/classes/Shirt.java
@@ -1,4 +1,24 @@
 package classes;
 
-public class Shirt {
+public class Shirt extends Clothes {
+
+    private int length;
+    private ClothingStyle style;
+
+    public ClothingStyle getStyle() {
+        return style;
+    }
+
+    public void setStyle(ClothingStyle style) {
+        this.style = style;
+    }
+
+    
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
